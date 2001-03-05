c1ab64aa64377dc31c246715458c8283082c3646
Eric Busboom <eric@clarinova.com>
2001-03-05 18:39:36 +0000

    Created derived properties

--- libical/src/python/DerivedProperties.py
+++ libical/src/python/DerivedProperties.py
@@ -7,7 +7,7 @@
 # DESCRIPTION:
 #   
 #
-#  $Id: DerivedProperties.py,v 1.1 2001-03-05 18:30:40 ebusboom Exp $
+#  $Id: DerivedProperties.py,v 1.2 2001-03-05 18:39:36 ebusboom Exp $
 #  $Locker:  $
 #
 # (C) COPYRIGHT 2001, Eric Busboom <eric@softwarestudio.org>
@@ -27,43 +27,33 @@
 
 from Property import *
 
-class RDate_Time(Time):
-    def __init__(self,arg): Time.__init__(self,arg,"RDATE")
+def RDate(arg):
+
+    class RDate_Time(Time):
+        def __init__(self,arg): Time.__init__(self,arg,"RDATE")
     
-class RDate_Period(Period):
-    def __init__(self,arg): Period.__init__(self,arg,"RDATE")
+    class RDate_Period(Period):
+        def __init__(self,arg): Period.__init__(self,arg,"RDATE")
 
-class RDate_Factory:
-    def __call__(self,arg):        
-        p = None
-        for c in [RDate_Time, RDate_Period]:
-            try: return c(arg)
-            except Property.ConstructorFailedError, d: pass
-        
-        raise Property.ConstructorFailedError("Failed to construct RDATE from "+str(arg))
-
-RDate = RDate_Factory()
+    p = None
+    for c in [RDate_Time, RDate_Period]:
+        try: return c(arg)
+        except Property.ConstructorFailedError, d: pass
+    raise Property.ConstructorFailedError("Failed to construct RDATE from "+str(arg))
 
 
-
-class Trigger_Time(Time):
-    def __init__(self,arg): Time.__init__(self,arg,"TRIGGER")
+def Trigger(arg):        
+    class Trigger_Time(Time): 
+        def __init__(self,arg): Time.__init__(self,arg,"TRIGGER")
     
-class Trigger_Duration(Duration):
-    def __init__(self,arg): Duration.__init__(self,arg,"TRIGGER")
+    class Trigger_Duration(Duration):
+        def __init__(self,arg): Duration.__init__(self,arg,"TRIGGER")
 
-class Trigger_Factory:
-    def __call__(self,arg):        
-        p = None
-        for c in [Trigger_Duration, Trigger_Time]:
-            try:
-                return c(arg)
-            except Property.ConstructorFailedError, d: 
-                pass
-        
-        raise Property.ConstructorFailedError("Failed to construct TRIGGER from "+str(arg))
+    p = None
+    for c in [Trigger_Duration, Trigger_Time]:
+        try: return c(arg)
+        except Property.ConstructorFailedError, d: pass        
+    raise Property.ConstructorFailedError("Failed to construct TRIGGER from "+str(arg))
 
-
-Trigger = Trigger_Factory()
 
 
