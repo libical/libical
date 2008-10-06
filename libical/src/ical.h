f2487a2e5fb41171207eff0bfbf2726fa40a3c28
Art Cancro <room_libical@uncensored.citadel.org>
2008-10-06 02:34:38 +0000

    Backed out r870 and part of r871. The copy of ical.h in src/ is *not* the generated file.  It is the file that is installed to /usr/include/ical.h which includes libical/ical.h and throws a compiler warning about the deprecated include path.  We can't take this out quite yet.

---
+++ libical/src/ical.h
@@ -0,0 +1,23 @@
+/************************************************************************
+
+ FILE:         ical.h
+ CREATOR:      ajc 2008-sep-01
+
+ (C) COPYRIGHT 2008 by Art Cancro
+ http://freeassociation.sourceforge.net
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of either: 
+
+    The LGPL as published by the Free Software Foundation, version
+    2.1, available at: http://www.fsf.org/copyleft/lesser.html
+
+  Or:
+
+    The Mozilla Public License Version 1.0. You may obtain a copy of
+    the License at http://www.mozilla.org/MPL/
+
+ ************************************************************************/
+
+#warning "#include <ical.h> is deprecated.  Please #include <libical/ical.h> instead."
+#include <libical/ical.h>
