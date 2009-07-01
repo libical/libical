533b361184302801eabe4cc1b1109dea7c8aecd6
Alvaro Manera <alvaro.manera@nokia.com>
2009-07-01 06:12:28 +0000

    Fixing some missing files licensing.

--- libical/src/libical/autogenex/icalrestriction.c
+++ libical/src/libical/autogenex/icalrestriction.c
@@ -1,7 +1,18 @@
 /*  -*- Mode: C -*- */
 /*  ======================================================================
   File: icalrestriction.c
-    
+
+     This program is free software; you can redistribute it and/or modify
+ it under the terms of either:
+
+    The LGPL as published by the Free Software Foundation, version
+    2.1, available at: http://www.fsf.org/copyleft/lesser.html
+
+  Or:
+
+    The Mozilla Public License Version 1.0. You may obtain a copy of
+    the License at http://www.mozilla.org/MPL/
+
  (C) COPYRIGHT 2000, Eric Busboom, http://www.softwarestudio.org
  ======================================================================*/
 /*#line 7 "icalrestriction.c.in"*/
