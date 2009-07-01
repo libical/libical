508b09ac1e419eccd420dde613a7e38589e92ba1
Alvaro Manera <alvaro.manera@nokia.com>
2009-07-01 06:12:28 +0000

    Fixing some missing files licensing.

--- libical/src/libicalss/icalcomponent.h
+++ libical/src/libicalss/icalcomponent.h
@@ -7,17 +7,19 @@
   (C) COPYRIGHT 1999 Eric Busboom 
   http://www.softwarestudio.org
 
-  The contents of this file are subject to the Mozilla Public License
-  Version 1.0 (the "License"); you may not use this file except in
-  compliance with the License. You may obtain a copy of the License at
-  http://www.mozilla.org/MPL/
- 
-  Software distributed under the License is distributed on an "AS IS"
-  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
-  the License for the specific language governing rights and
-  limitations under the License.
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of either:
 
-  The original author is Eric Busboom
+    The LGPL as published by the Free Software Foundation, version
+    2.1, available at: http://www.fsf.org/copyleft/lesser.html
+
+  Or:
+
+    The Mozilla Public License Version 1.0. You may obtain a copy of
+    the License at http://www.mozilla.org/MPL/
+
+ The Original Code is eric. The Initial Developer of the Original
+ Code is Eric Busboom
   The original code is icalcomponent.h
 
 ======================================================================*/
