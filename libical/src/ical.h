ed3396388f808ba430278c30da59d5a5c329200d
Art Cancro <room_libical@uncensored.citadel.org>
2011-07-11 13:21:50 +0000

    Some documentation pointed to URLs etc that led to the wrong license version numbers.  Corrected.

--- libical/src/ical.h
+++ libical/src/ical.h
@@ -9,13 +9,11 @@
  This program is free software; you can redistribute it and/or modify
  it under the terms of either:
 
-    The LGPL as published by the Free Software Foundation, version
-    2.1, available at: http://www.fsf.org/copyleft/lesser.html
+    The LGPL as published by the Free Software Foundation, version 2.1
 
   Or:
 
-    The Mozilla Public License Version 1.0. You may obtain a copy of
-    the License at http://www.mozilla.org/MPL/
+    The Mozilla Public License Version 1.0.
 
  ************************************************************************/
 
