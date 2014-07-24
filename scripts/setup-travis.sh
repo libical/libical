8c816c5012bca0f6ae0a0c78989d34a1e7b1ebe4
Allen Winter <allen.winter@kdab.com>
2014-07-24 15:15:42 -0400

    scripts/setup-travis.sh - no longer need to install cmake for Mac

--- scripts/setup-travis.sh
+++ scripts/setup-travis.sh
@@ -4,8 +4,10 @@
 
 if ( test "`uname -s`" = "Darwin" )
 then
-  brew update
-  brew install cmake
+  #cmake v2.8.12 is installed on the Mac workers now
+  #brew update
+  #brew install cmake
+  echo
 else
   #install a newer cmake since at this time Travis only has version 2.8.7
   echo "yes" | sudo add-apt-repository ppa:kalakris/cmake
