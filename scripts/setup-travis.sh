c6409d1740846d9fc5618f824c4befb1995bf45e
Allen Winter <allen.winter@kdab.com>
2014-10-11 10:07:58 -0400

    Copy in the RSCALE support feature

--- scripts/setup-travis.sh
+++ scripts/setup-travis.sh
@@ -13,4 +13,6 @@ else
   echo "yes" | sudo add-apt-repository ppa:kalakris/cmake
   sudo apt-get update -qq
   sudo apt-get install cmake
+  #comment out for now as it causes an Exception: SegFault in the regression test
+  #sudo apt-get install libicu-dev
 fi
