f7219fba3c5acb81267c2f3639b8301732c822a5
Allen Winter <allen.winter@kdab.com>
2014-10-27 10:57:23 -0400

    eliminate subscripts warning

--- src/libical/icalrecur.c
+++ src/libical/icalrecur.c
@@ -1248,7 +1248,7 @@ static int initialize_iterator(icalrecur_iterator* impl)
 	/* Create locale for RSCALE calendar (lowercasing) */
 	char *r, *l, *locale = icalmemory_tmp_buffer(strlen(rule.rscale) + 11);
 	strcpy(locale, "@calendar=");
-	for (r = rule.rscale, l = locale+10; *r; *l++ = tolower(*r++));
+	for (r = rule.rscale, l = locale+10; *r; *l++ = tolower((int)*r++));
 	*l = '\0';  /* EOS */
 
 	/* Create RSCALE calendar and set to DTSTART */
