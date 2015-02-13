51a8206d5ad25326f2fd3204c5f20593effef3a4
Ken Murchison <murch@andrew.cmu.edu>
2015-02-13 14:22:58 -0500

    icalrecur.h: tweak BYxxx array sizes

--- src/libical/icalrecur.h
+++ src/libical/icalrecur.h
@@ -128,15 +128,15 @@ enum {
  *
  * The maximums below are based on Chinese/Hebrew leap years (13 months)
  */
-#define ICAL_BY_SECOND_SIZE 61
-#define ICAL_BY_MINUTE_SIZE 61
-#define ICAL_BY_HOUR_SIZE 25
-#define ICAL_BY_DAY_SIZE 385 /* 7 days * 55 weeks */
-#define ICAL_BY_MONTHDAY_SIZE 32
-#define ICAL_BY_YEARDAY_SIZE 386
-#define ICAL_BY_WEEKNO_SIZE 57
-#define ICAL_BY_MONTH_SIZE 14
-#define ICAL_BY_SETPOS_SIZE 386
+#define ICAL_BY_SECOND_SIZE	62	/* 0 to 60 */
+#define ICAL_BY_MINUTE_SIZE	61	/* 0 to 59 */	
+#define ICAL_BY_HOUR_SIZE	25	/* 0 to 23 */
+#define ICAL_BY_DAY_SIZE	7*ICAL_BY_WEEKNO_SIZE
+#define ICAL_BY_MONTHDAY_SIZE	32	/* 1 to 31 */
+#define ICAL_BY_YEARDAY_SIZE	386	/* 1 to 385 */
+#define ICAL_BY_WEEKNO_SIZE	56	/* 1 to 55 */
+#define ICAL_BY_MONTH_SIZE	14	/* 1 to 13 */
+#define ICAL_BY_SETPOS_SIZE	ICAL_BY_YEARDAY_SIZE
 
 /** Main struct for holding digested recurrence rules */
 struct icalrecurrencetype 
