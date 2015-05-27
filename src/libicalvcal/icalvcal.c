0114e52ba5484958e5e019ec014d9de2d010e728
Etienne CHAMPETIER <etienne.champetier@fiducial.net>
2015-05-27 15:20:13 +0200

    remove some Dead store
    spotted with scan-build (clang/llvm)
    
    Signed-off-by: Etienne CHAMPETIER <etienne.champetier@fiducial.net>

--- src/libicalvcal/icalvcal.c
+++ src/libicalvcal/icalvcal.c
@@ -1213,38 +1213,38 @@ static void* rule_prop(int icaltype, VObject *object, icalcomponent *comp,
       /* The DAILY RRULE only has an interval and duration (COUNT/UNTIL). */
         recur.freq = ICAL_DAILY_RECURRENCE;
         p = rrule_parse_interval (s + 1, &recur, &error_message);
-        p = rrule_parse_duration (p, &recur, &error_message);
+        rrule_parse_duration (p, &recur, &error_message);
     } else if (*s == 'W') {
         /* The WEEKLY RRULE has weekday modifiers - MO TU WE. */
         recur.freq = ICAL_WEEKLY_RECURRENCE;
         p = rrule_parse_interval (s + 1, &recur, &error_message);
         p = rrule_parse_weekly_days (p, &recur, &error_message);
-        p = rrule_parse_duration (p, &recur, &error_message);
+        rrule_parse_duration (p, &recur, &error_message);
     } else if (*s == 'M' && *(s + 1) == 'D') {
         /* The MONTHLY By Day RRULE has day number modifiers - 1 1- LD. */
         recur.freq = ICAL_MONTHLY_RECURRENCE;
         p = rrule_parse_interval (s + 2, &recur, &error_message);
         p = rrule_parse_monthly_days (p, &recur, &error_message);
-        p = rrule_parse_duration (p, &recur, &error_message);
+        rrule_parse_duration (p, &recur, &error_message);
     } else if (*s == 'M' && *(s + 1) == 'P') {
         /* The MONTHLY By Position RRULE has position modifiers - 1 2- and
            weekday modifiers - MO TU. */
         recur.freq = ICAL_MONTHLY_RECURRENCE;
         p = rrule_parse_interval (s + 2, &recur, &error_message);
         p = rrule_parse_monthly_positions (p, &recur, &error_message);
-        p = rrule_parse_duration (p, &recur, &error_message);
+        rrule_parse_duration (p, &recur, &error_message);
     } else if (*s == 'Y' && *(s + 1) == 'M') {
         /* The YEARLY By Month RRULE has month modifiers - 1 3 12. */
         recur.freq = ICAL_YEARLY_RECURRENCE;
         p = rrule_parse_interval (s + 2, &recur, &error_message);
         p = rrule_parse_yearly_months (p, &recur, &error_message);
-        p = rrule_parse_duration (p, &recur, &error_message);
+        rrule_parse_duration (p, &recur, &error_message);
     } else if (*s == 'Y' && *(s + 1) == 'D') {
         /* The YEARLY By Day RRULE has day number modifiers - 100 200. */
         recur.freq = ICAL_YEARLY_RECURRENCE;
         p = rrule_parse_interval (s + 2, &recur, &error_message);
         p = rrule_parse_yearly_days (p, &recur, &error_message);
-        p = rrule_parse_duration (p, &recur, &error_message);
+        rrule_parse_duration (p, &recur, &error_message);
     } else {
         error_message = "Invalid RRULE Frequency";
     }
