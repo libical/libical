8dc6cf10f559270fc3890593d1852a017cef8e63
Allen Winter <allen.winter@kdab.com>
2016-01-25 17:47:09 -0500

    icalversion.h.cmake - add macros for ICAL_CHECK_VERSION and friends ICAL_MAJOR_VERSION, ICAL_MINOR_VERSION, ICAL_MICRO_VERSION as requested by Milan Crha.

--- src/libical/icalversion.h.cmake
+++ src/libical/icalversion.h.cmake
@@ -1,7 +1,46 @@
+/*======================================================================
+ FILE: icalversion.h
+ CREATOR: eric 20 March 1999
+
+ (C) COPYRIGHT 2000, Eric Busboom <eric@softwarestudio.org>
+     http://www.softwarestudio.org
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of either:
+
+    The LGPL as published by the Free Software Foundation, version
+    2.1, available at: http://www.gnu.org/licenses/lgpl-2.1.html
+
+ Or:
+
+    The Mozilla Public License Version 1.0. You may obtain a copy of
+    the License at http://www.mozilla.org/MPL/
+ ======================================================================*/
+
 #ifndef ICAL_VERSION_H
 #define ICAL_VERSION_H
 
 #define ICAL_PACKAGE "@CMAKE_PROJECT_NAME@"
 #define ICAL_VERSION "@PROJECT_VERSION@"
+
+#define ICAL_MAJOR_VERSION (@LIBICAL_LIB_MAJOR_VERSION@)
+#define ICAL_MINOR_VERSION (@LIBICAL_LIB_MINOR_VERSION@)
+#define ICAL_PATCH_VERSION (@LIBICAL_LIB_PATCH_VERSION@)
+#define ICAL_MICRO_VERSION ICAL_PATCH_VERSION
+
+/**
+ * ICAL_CHECK_VERSION:
+ * @param major: major version (e.g. 1 for version 1.2.5)
+ * @param minor: minor version (e.g. 2 for version 1.2.5)
+ * @param micro: micro version (e.g. 5 for version 1.2.5)
+ *
+ * @return TRUE if the version of the LIBICAL header files
+ * is the same as or newer than the passed-in version.
+ */
+#define ICAL_CHECK_VERSION(major,minor,micro)                          \
+    (ICAL_MAJOR_VERSION > (major) ||                                   \
+    (ICAL_MAJOR_VERSION == (major) && ICAL_MINOR_VERSION > (minor)) || \
+    (ICAL_MAJOR_VERSION == (major) && ICAL_MINOR_VERSION == (minor) && \
+    ICAL_MICRO_VERSION >= (micro)))
 
 #endif
