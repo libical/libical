660d71810273d46a9ae6f78163abb320b7f11bb2
Allen Winter <allen.winter@kdab.com>
2016-08-10 16:40:16 -0400

    scripts/set_compiler_env.bat - adapt %PATH% for MinGW can't have sh.ex in path or CMake will not work

--- scripts/set_compiler_env.bat
+++ scripts/set_compiler_env.bat
@@ -12,23 +12,23 @@ set arch=x86
 if "%platform%" EQU "x64" ( set arch=x86_amd64 )
 
 if "%Configuration%"=="2015" (
-	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+        set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 )
 
 if "%Configuration%"=="2013" (
-	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+        set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
 )
 
 if "%Configuration%"=="2012" (
-	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
+        set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
 )
 
 if "%Configuration%"=="2010" (
-	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+        set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 )
 
 if "%Configuration%"=="2008" (
-	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
+        set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
 )
 
 :: Visual Studio detected
@@ -37,4 +37,7 @@ goto :eof
 
 :: MinGW detected
 :mingw
+:: workaround for CMake not wanting sh.exe in PATH for MinGW
+set PATH=%PATH:C:\Program Files (x86)\Git\bin;=%
+
 endlocal & set PATH=c:\mingw\bin;%PATH%
