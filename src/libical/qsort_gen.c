2d3daa499a00592e2dbf4ae43816481a3b22d146
Markus Minichmayr <markus@tapkey.com>
2018-03-08 15:58:00 +0100

    Extended qsort/introduced qsort_gen that allows a more generic use with lists that are not stored in single contiguous blocks of memory.

--- src/libical/qsort_gen.c
+++ src/libical/qsort_gen.c
@@ -1,25 +1,50 @@
-/* qsort( void *, size_t, size_t, int(*)( const void *, const void * ) )
+/*======================================================================
+ FILE: qsort_flex.h
 
-   This file is part of the Public Domain C Library (PDCLib).
-   Permission is granted to use, modify, and / or redistribute at will.
-*/
+ This library is free software; you can redistribute it and/or modify
+ it under the terms of either:
+
+    The LGPL as published by the Free Software Foundation, version
+    2.1, available at: http://www.gnu.org/licenses/lgpl-2.1.html
+
+ Or:
+
+    The Mozilla Public License Version 2.0. You may obtain a copy of
+    the License at http://www.mozilla.org/MPL/
+
+ The code in this file was initially authored as part of the PDCLib project
+ and placed in the public domain. The initial copyright notice was as follows:
+
+    "This file is part of the Public Domain C Library (PDCLib).
+    Permission is granted to use, modify, and / or redistribute at will.
+    
+    This implementation is taken from Paul Edward's PDPCLIB.
+
+    Original code is credited to Raymond Gardner, Englewood CO.
+    Minor mods are credited to Paul Edwards.
+    Some reformatting and simplification done by Martin Baute."
+
+ Modifications have been made by Markus Minichmayr to allow more generic use.
+======================================================================*/
+
 
 #include <stdlib.h>
 
-#ifndef REGTEST
-
-/* This implementation is taken from Paul Edward's PDPCLIB.
-
-   Original code is credited to Raymond Gardner, Englewood CO.
-   Minor mods are credited to Paul Edwards.
-   Some reformatting and simplification done by Martin Baute.
-   All code is still Public Domain.
-*/
-
 /* Wrapper for _PDCLIB_memswp protects against multiple argument evaluation. */
-static inline void memswp( char * i, char * j, size_t size )
+void qsort_gen_memswap(void *m1, void *m2, size_t size)
 {
-    _PDCLIB_memswp( i, j, size );
+    char *p1 = (char *) m1;
+    char *p2 = (char *) m2;
+    char tmp;
+
+    while (size) {
+        tmp = *p1;
+        *p1 = *p2;
+        *p2 = tmp;
+        size--;
+        p1++;
+        p2++;
+    }
 }
 
 /* For small sets, insertion sort is faster than quicksort.
@@ -29,63 +54,62 @@ static inline void memswp( char * i, char * j, size_t size )
 #define T 7
 
 /* Macros for handling the QSort stack */
-#define PREPARE_STACK char * stack[STACKSIZE]; char * * stackptr = stack
+#define PREPARE_STACK size_t stack[STACKSIZE]; size_t* stackptr = stack
 #define PUSH( base, limit ) stackptr[0] = base; stackptr[1] = limit; stackptr += 2
 #define POP( base, limit ) stackptr -= 2; base = stackptr[0]; limit = stackptr[1]
 /* TODO: Stack usage is log2( nmemb ) (minus what T shaves off the worst case).
          Worst-case nmemb is platform dependent and should probably be 
-         configured through _PDCLIB_config.h.
+         configured.
 */
 #define STACKSIZE 64
 
-void qsort( void * base, size_t nmemb, size_t size, int (*compar)( const void *, const void * ) )
+void qsort_gen(void* list, size_t nitems, int(*compar) (const void *, size_t, size_t), void(*swapr) (void *, size_t, size_t))
 {
-    char * i;
-    char * j;
-    _PDCLIB_size_t thresh = T * size;
-    char * base_          = (char *)base;
-    char * limit          = base_ + nmemb * size;
+    size_t i;
+    size_t j;
+    size_t base_          = 0;
+    size_t limit          = nitems;
     PREPARE_STACK;
 
     for ( ;; )
     {
-        if ( (size_t)( limit - base_ ) > thresh ) /* QSort for more than T elements. */
+        if ( ( limit - base_ ) > T ) /* QSort for more than T elements. */
         {
             /* We work from second to last - first will be pivot element. */
-            i = base_ + size;
-            j = limit - size;
+            i = base_ + 1;
+            j = limit - 1;
             /* We swap first with middle element, then sort that with second
                and last element so that eventually first element is the median
                of the three - avoiding pathological pivots.
                TODO: Instead of middle element, chose one randomly.
             */
-            memswp( ( ( ( (size_t)( limit - base_ ) ) / size ) / 2 ) * size + base_, base_, size );
-            if ( compar( i, j ) > 0 ) memswp( i, j, size );
-            if ( compar( base_, j ) > 0 ) memswp( base_, j, size );
-            if ( compar( i, base_ ) > 0 ) memswp( i, base_, size );
+            swapr( list, ( limit - base_ ) / 2 + base_, base_);
+            if ( compar( list, i, j ) > 0 ) swapr( list, i, j );
+            if ( compar( list, base_, j ) > 0 ) swapr( list, base_, j );
+            if ( compar( list, i, base_ ) > 0 ) swapr( list, i, base_ );
             /* Now we have the median for pivot element, entering main Quicksort. */
             for ( ;; )
             {
                 do
                 {
                     /* move i right until *i >= pivot */
-                    i += size;
-                } while ( compar( i, base_ ) < 0 );
+                    i ++;
+                } while ( compar( list, i, base_ ) < 0 );
                 do
                 {
                     /* move j left until *j <= pivot */
-                    j -= size;
-                } while ( compar( j, base_ ) > 0 );
+                    j --;
+                } while ( compar( list, j, base_ ) > 0 );
                 if ( i > j )
                 {
                     /* break loop if pointers crossed */
                     break;
                 }
                 /* else swap elements, keep scanning */
-                memswp( i, j, size );
+                swapr( list, i, j );
             }
             /* move pivot into correct place */
-            memswp( base_, j, size );
+            swapr( list, base_, j );
             /* larger subfile base / limit to stack, sort smaller */
             if ( j - base_ > limit - i )
             {
@@ -102,11 +126,11 @@ void qsort( void * base, size_t nmemb, size_t size, int (*compar)( const void *,
         }
         else /* insertion sort for less than T elements              */
         {
-            for ( j = base_, i = j + size; i < limit; j = i, i += size )
+            for ( j = base_, i = j + 1; i < limit; j = i, i++ )
             {
-                for ( ; compar( j, j + size ) > 0; j -= size )
+                for ( ; compar( list, j, j + 1 ) > 0; j -- )
                 {
-                    memswp( j, j + size, size );
+                    swapr( list, j, j + 1 );
                     if ( j == base_ )
                     {
                         break;
@@ -124,41 +148,3 @@ void qsort( void * base, size_t nmemb, size_t size, int (*compar)( const void *,
         }
     }
 }
-
-#endif
-
-#ifdef TEST
-#include "_PDCLIB_test.h"
-#include <string.h>
-#include <limits.h>
-
-static int compare( const void * left, const void * right )
-{
-    return *( (unsigned char *)left ) - *( (unsigned char *)right );
-}
-
-int main( void )
-{
-    char presort[] = { "shreicnyjqpvozxmbt" };
-    char sorted1[] = { "bcehijmnopqrstvxyz" };
-    char sorted2[] = { "bticjqnyozpvreshxm" };
-    char s[19];
-    strcpy( s, presort );
-    qsort( s, 18, 1, compare );
-    TESTCASE( strcmp( s, sorted1 ) == 0 );
-    strcpy( s, presort );
-    qsort( s, 9, 2, compare );
-    TESTCASE( strcmp( s, sorted2 ) == 0 );
-    strcpy( s, presort );
-    qsort( s, 1, 1, compare );
-    TESTCASE( strcmp( s, presort ) == 0 );
-#if defined(REGTEST) && (__BSD_VISIBLE || __APPLE__)
-    puts( "qsort.c: Skipping test #4 for BSD as it goes into endless loop here." );
-#else
-    qsort( s, 100, 0, compare );
-    TESTCASE( strcmp( s, presort ) == 0 );
-#endif
-    return TEST_RESULTS;
-}
-
-#endif
