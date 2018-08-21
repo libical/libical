5bf34198f83cb2f00f34044c33c80cdf6a1e1985
Allen Winter <allen.winter@kdab.com>
2018-08-21 09:27:48 -0400

    Revert "appveyor - trying to support more compilers and platforms"
    This reverts commit 5a2fdd92346305d3b6992ee379f258323557d5c1.

--- scripts/set_compiler_env.bat
+++ scripts/set_compiler_env.bat
@@ -3,48 +3,39 @@
 :: Now we declare a scope
 Setlocal EnableDelayedExpansion EnableExtensions
 
-if not defined COMPILER set COMPILER=msvc2015
+if not defined Configuration set Configuration=2015
 
-if "%COMPILER%"=="MinGW" ( goto :mingw )
+if "%Configuration%"=="MinGW" ( goto :mingw )
 
-set VCARCH=x86
+set arch=x86
 
-if "%PLATFORM%"=="X64" ( set VCARCH=x86_amd64 )
+if "%platform%" EQU "x64" ( set arch=x86_amd64 )
 
-if "%COMPILER%"=="msvc2015" (
-    set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+if "%Configuration%"=="2015" (
+        set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
 )
 
-if "%COMPILER%"=="msvc2013" (
-    set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+if "%Configuration%"=="2013" (
+        set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
 )
 
-if "%COMPILER%"=="msvc2012" (
-    set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
+if "%Configuration%"=="2012" (
+        set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
 )
 
-if "%COMPILER%"=="msvc2010" (
-    set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+if "%Configuration%"=="2010" (
+        set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 )
 
-if "%COMPILER%"=="msvc2008" (
-    set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
+if "%Configuration%"=="2008" (
+        set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
 )
 
 :: Visual Studio detected
-endlocal & call %SET_VS_ENV% %VCARCH%
+endlocal & call %SET_VS_ENV% %arch%
 goto :eof
 
 :: MinGW detected
 :mingw
 :: workaround for CMake not wanting sh.exe in PATH for MinGW
-endlocal & set PATH=%PATH:C:\Program Files\Git\usr\bin;=% & set "PATH=c:\MinGW\bin;%PATH%"
-
-:eof
-if "%CMAKE_GENERATOR%"=="Ninja" (
-    cinst -y ninja
-    set PATH=C:\tools\ninja;%PATH%
-)
-if "%CMAKE_GENERATOR%"=="NMake Makefiles JOM" (
-    cinst -y jom
-)
+endlocal & set PATH=%PATH:C:\Program Files\Git\usr\bin;=% & set PATH=c:\MinGW\bin;%PATH%
