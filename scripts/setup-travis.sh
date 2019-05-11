feabecaa3e38579aec13b36892cbf9b3111f4457
Allen Winter <allen.winter@kdab.com>
2019-05-11 12:06:05 -0400

    scripts/setup-travis.sh - install cmake 3.11.0

--- scripts/setup-travis.sh
+++ scripts/setup-travis.sh
@@ -2,6 +2,10 @@
 
 # things to do for travis-ci in the before_install section
 
+# Install a supported cmake version (>= 3.11.0)
+wget -O cmake.sh https://cmake.org/files/v3.11/cmake-3.11.0-Linux-x86_64.sh 
+sudo sh cmake.sh --skip-license --exclude-subdir --prefix=/usr/local
+
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   brew install libxml2
   brew install icu4c
