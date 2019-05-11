7f017d07cd88b65b9d894078e32ff7f1ef08baeb
Allen Winter <allen.winter@kdab.com>
2019-05-11 12:33:53 -0400

    more travis changes trying to get it to find cmake 3.11

--- scripts/setup-travis.sh
+++ scripts/setup-travis.sh
@@ -2,17 +2,17 @@
 
 # things to do for travis-ci in the before_install section
 
-# Install a supported cmake version (>= 3.11.0)
-wget -O cmake.sh https://cmake.org/files/v3.11/cmake-3.11.0-Linux-x86_64.sh 
-sudo sh cmake.sh --skip-license --exclude-subdir --prefix=/usr/local
-
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+  brew install cmake
   brew install libxml2
   brew install icu4c
   brew install db
   brew install gobject-introspection
   brew install gtk-doc
 else
+  # Install a supported cmake version (>= 3.11.0)
+  wget -O cmake.sh https://cmake.org/files/v3.11/cmake-3.11.0-Linux-x86_64.sh 
+  sudo sh cmake.sh --skip-license --exclude-subdir --prefix=/usr/local
   sudo apt-get update -qq
   sudo apt-get install libicu-dev
   sudo apt-get install libdb-dev
