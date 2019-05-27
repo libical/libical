e84714e9d6ec724dca889531e11fb963cadc2dba
Allen Winter <allen.winter@kdab.com>
2019-05-27 17:50:56 -0400

    CMakeLists.txt - new macros for C and C++ compiler flag checking
    Issue#386

---
+++ cmake/modules/LibIcalMacrosInternal.cmake
@@ -0,0 +1,22 @@
+# CMake support macros and functions for the libical project
+
+include(CheckCCompilerFlag)
+include(CheckCXXCompilerFlag)
+
+function(libical_append_if condition value)
+  if(${condition})
+    foreach(variable ${ARGN})
+      set(${variable} "${${variable}} ${value}" PARENT_SCOPE)
+    endforeach()
+  endif()
+endfunction()
+
+macro(libical_add_cflag flag name)
+  check_c_compiler_flag("${flag}" "C_SUPPORTS_${name}")
+  libical_append_if("C_SUPPORTS_${name}" "${flag}" CMAKE_C_FLAGS)
+endmacro()
+
+macro(libical_add_cxxflag flag name)
+  check_cxx_compiler_flag("${flag}" "CXX_SUPPORTS_${name}")
+  libical_append_if("CXX_SUPPORTS_${name}" "${flag}" CMAKE_CXX_FLAGS)
+endmacro()
