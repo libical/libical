06056971efaa6ad1a2b30067564d94371337581c
Ken Murchison <murch@fastmail.com>
2019-06-14 16:56:23 -0400

    Revert "icalrecur.[ch]: move all ICAL_BY_*_SIZE macros into icalrecur.h"
    This reverts commit 0240d6f3bb7b09c37e5339201d112ed837850541.

--- src/libical/icalrecur.c
+++ src/libical/icalrecur.c
@@ -146,6 +146,15 @@
 #if defined(HAVE_LIBICU)
 #include <unicode/ucal.h>
 #include <unicode/ustring.h>
+#else
+
+/* The maximums below are based on Gregorian leap years */
+#undef ICAL_BY_MONTH_SIZE
+#undef ICAL_BY_WEEKNO_SIZE
+#undef ICAL_BY_YEARDAY_SIZE
+#define ICAL_BY_MONTH_SIZE      13      /* 1 to 12 */
+#define ICAL_BY_WEEKNO_SIZE     54      /* 1 to 53 */
+#define ICAL_BY_YEARDAY_SIZE    367     /* 1 to 366 */
 #endif
 
 #if (SIZEOF_TIME_T > 4)
