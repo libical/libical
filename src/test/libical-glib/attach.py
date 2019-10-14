57e4b56fac028f83b7a467001ea982b47ab44580
Milan Crha <mcrha@redhat.com>
2019-10-14 21:23:20 +0200

    Let icalattach_new_from_data() use the 'free_fn' argument again
    This had been disabled with commit 7a2f318bacae5521848963d96a3a644bc6be1d01
    (in time of 1.0.1) to fix inline attachments crashing. That commit could
    eventually cause memory leaks and higher memory usage.
    
    This change adds necessary strdup() calls on places where required only and
    makes use of the 'free_fn' argument again. It also fixes libical-glib's
    i_cal_attach_new_from_bytes(), which relies on this 'free_fn'.

--- src/test/libical-glib/attach.py
+++ src/test/libical-glib/attach.py
@@ -33,7 +33,7 @@ assert(attach_url.get_is_url() == 1);
 retrieved_url = attach_url.get_url();
 assert(retrieved_url == dummy_url);
 
-attach_data = ICalGLib.Attach.new_from_data(dummy_data, ICalGLib.memory_free_buffer, None);
+attach_data = ICalGLib.Attach.new_from_data(dummy_data, None, None);
 assert(attach_data.get_is_url() == 0);
 retrieved_data = attach_data.get_data();
 assert(retrieved_data == dummy_data);
