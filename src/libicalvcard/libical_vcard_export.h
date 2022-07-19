f839975e08a15fc71a6cdf66cd0e85f7b6278ac8
Ken Murchison <murch@fastmail.com>
2022-07-19 14:20:21 -0400

    Rename LIBICALVCARD[_NO]_EXPORT to LIBICAL_VCARD[_NO]_EXPORT

--- src/libicalvcard/libical_vcard_export.h
+++ src/libicalvcard/libical_vcard_export.h
@@ -1,24 +1,24 @@
-#ifndef LIBICALVCARD_EXPORT_H
-#define LIBICALVCARD_EXPORT_H
+#ifndef LIBICAL_VCARD_EXPORT_H
+#define LIBICAL_VCARD_EXPORT_H
 
 #if !defined(S_SPLINT_S)
 
-#ifdef LIBICALVCARD_STATIC_DEFINE
-#define LIBICALVCARD_EXPORT
-#define LIBICALVCARD_NO_EXPORT
+#ifdef LIBICAL_VCARD_STATIC_DEFINE
+#define LIBICAL_VCARD_EXPORT
+#define LIBICAL_VCARD_NO_EXPORT
 #else
 #if defined(_MSC_VER) || defined(__CYGWIN__)
 #if defined(libical_ical_EXPORTS)
        /* We are building this library */
-#define LIBICALVCARD_EXPORT __declspec(dllexport)
+#define LIBICAL_VCARD_EXPORT __declspec(dllexport)
 #else
        /* We are using this library */
-#define LIBICALVCARD_EXPORT __declspec(dllimport)
+#define LIBICAL_VCARD_EXPORT __declspec(dllimport)
 #endif
-#define LIBICALVCARD_NO_EXPORT
+#define LIBICAL_VCARD_NO_EXPORT
 #else
-#define LIBICALVCARD_EXPORT __attribute__((visibility("default")))
-#define LIBICALVCARD_NO_EXPORT __attribute__((visibility("hidden")))
+#define LIBICAL_VCARD_EXPORT __attribute__((visibility("default")))
+#define LIBICAL_VCARD_NO_EXPORT __attribute__((visibility("hidden")))
 #endif
 #endif
 
