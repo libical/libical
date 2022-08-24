227dce9b2a3bce261b049d931d372408b69dd752
Ken Murchison <murch@fastmail.com>
2022-08-24 15:56:36 -0400

    vcardvalueimpl.h: remove iCalendar enumeration types

--- src/libicalvcard/vcardvalueimpl.h
+++ src/libicalvcard/vcardvalueimpl.h
@@ -21,9 +21,9 @@ struct vcardvalue_impl
     union data
     {
         const char *v_string;
-        /*char *v_text; */      /* use v_string */
-        /*char *v_language_tag; *//* use v_string */
-        /*char *v_uri; */       /* use v_string */
+        /*char *v_text; */         /* use v_string */
+        /*char *v_language_tag; */ /* use v_string */
+        /*char *v_uri; */          /* use v_string */
 
         float v_float;
 
@@ -33,22 +33,15 @@ struct vcardvalue_impl
         /*int v_utcoffset; *//* use v_int */
 
 //        struct vcardtimetype v_time;
-        /*struct vcardtimetype v_date; */         /* use v_time */
-        /*struct vcardtimetype v_time; */         /* use v_time */
-        /*struct vcardtimetype v_datetime; */     /* use v_time */
-        /*struct vcardtimetype v_dateandortime; *//* use v_time */
+        /*struct vcardtimetype v_date; */          /* use v_time */
+        /*struct vcardtimetype v_time; */          /* use v_time */
+        /*struct vcardtimetype v_datetime; */      /* use v_time */
+        /*struct vcardtimetype v_dateandortime; */ /* use v_time */
 
         int v_enum;
         /* v_enum takes care of several enumerated types including:
-           vcardproperty_method v_method;
-           vcardproperty_status v_status;
-           vcardproperty_action v_action;
-           vcardproperty_class v_class;
-           vcardproperty_transp v_transp;
-           vcardproperty_busytype v_busytype;
-           vcardproperty_taskmode v_taskmode;
-           vcardproperty_pollmode v_pollmode;
-           vcardproperty_pollcompletion v_pollcomplete;
+           vcardproperty_kind    v_kind;
+           vcardproperty_version v_version;
          */
 
     } data;
