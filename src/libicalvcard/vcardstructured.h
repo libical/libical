672b641c43327d5a09b063b047e54e1ba25cfb01
Ken Murchison <murch@fastmail.com>
2023-04-27 09:12:35 -0400

    Move vcardstructuredtype to vcardstructured.[ch]

---
+++ src/libicalvcard/vcardstructured.h
@@ -0,0 +1,19 @@
+#ifndef VCARDSTRUCTURED_H
+#define VCARDSTRUCTURED_H
+
+#include "libical_vcard_export.h"
+
+#include "vcardstrarray.h"
+
+#define VCARD_MAX_STRUCTURED_FIELDS 10  // ADR currently uses 7
+
+typedef struct vcardstructuredtype {
+    unsigned num_fields;
+    vcardstrarray *field[VCARD_MAX_STRUCTURED_FIELDS];
+} vcardstructuredtype;
+
+LIBICAL_VCARD_EXPORT vcardstructuredtype *vcardstructured_new();
+
+LIBICAL_VCARD_EXPORT void vcardstructured_free(vcardstructuredtype *s);
+
+#endif /* VCARDSTRUCTURED_H */
