c70aab93ab9fa2414e586cbc508f81dc29476f13
Ken Murchison <murch@fastmail.com>
2023-05-11 13:59:26 -0400

    vcardparameter.c: serialize structured parameter values (RANKS)
    moved type-specific value serialization functions into their
    respective type source files

--- src/libicalvcard/vcardstructured.c
+++ src/libicalvcard/vcardstructured.c
@@ -21,11 +21,33 @@ vcardstructuredtype *vcardstructured_new()
     return s;
 }
 
+char *vcardstructured_as_vcard_string_r(vcardstructuredtype *s)
+{
+    vcardstrarray *array;
+    char *buf, *buf_ptr;
+    size_t buf_size;
+    unsigned i, num_fields;
+
+    num_fields = s->num_fields;
+    buf_size = num_fields * 25;  // arbitrary
+    buf_ptr = buf = icalmemory_new_buffer(buf_size);
+
+    for (i = 0; i < num_fields; i++) {
+        array = s->field[i];
+        if (i) icalmemory_append_char(&buf, &buf_ptr, &buf_size, ';');
+        if (array)
+            vcardstrarray_as_vcard_string_r(array, ',',
+                                            &buf, &buf_ptr, &buf_size);
+    }
+
+    return buf;
+}
+
 void vcardstructured_free(vcardstructuredtype *s)
 {
     unsigned i;
 
     for (i = 0; i < s->num_fields; i++)
-        vcardstrarray_free(s->field[i]);
+        if (s->field[i]) vcardstrarray_free(s->field[i]);
     icalmemory_free_buffer((void *)s);
 }
