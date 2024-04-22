71744bca23d8120048bf07077063c99abd9e7acf
Ken Murchison <murch@fastmail.com>
2024-04-22 07:58:21 -0400

    vcard_test.c: init data buffer

--- src/test/libicalvcard/vcard_test.c
+++ src/test/libicalvcard/vcard_test.c
@@ -52,6 +52,7 @@ int main(int argc, const char **argv)
 
     fstat(fd, &sbuf);
     data = malloc(sbuf.st_size+1);
+    memset(data, 0, sbuf.st_size+1);
 
     int r = read(fd, data, sbuf.st_size);
     if (r < 0) {
@@ -60,7 +61,6 @@ int main(int argc, const char **argv)
         close(fd);
         return -1;
     }
-    data[r+1] = '\0';
 
     vcardcomponent *card = vcardparser_parse_string(data);
     free(data);
