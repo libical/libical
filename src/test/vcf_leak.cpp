e3acef2e4584953d8d7091c2ad66de3cf40a12bc
Allen Winter <winter@kde.org>
2024-06-07 09:14:03 -0400

    vcf_leak.cpp - add a program to test for leaks in vcal
    Taken from the test program provided in issue #692

---
+++ src/test/vcf_leak.cpp
@@ -0,0 +1,52 @@
+/*======================================================================
+ *
+ * SPDX-FileCopyrightText: 2024 Contributors to the libical project <git@github.com:libical/libical>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-only OR MPL-2.0
+ *
+ * Based on the original code by Vladyslav in
+ * https://github.com/libical/libical/issues/692
+ *
+ * ======================================================================*/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#if defined(NDEBUG)
+#undef NDEBUG
+#endif
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <memory>
+#include <vector>
+
+#include "libical/ical.h"
+#include "vcc.h"
+#include "vobject.h"
+
+int main(int argc, char *argv[])
+{
+    const char *fname;
+
+    if (argc != 2) {
+        fname = TEST_DATADIR "/leaktest.vcf";
+    } else {
+        fname = argv[1];
+    }
+
+    std::string content;
+    {
+        std::ifstream myfile(fname);
+        std::stringstream ss;
+        ss << myfile.rdbuf();
+        content = ss.str();
+    }
+
+    using vcardptr = std::unique_ptr<VObject, decltype(&cleanVObject)>;
+    vcardptr ptr(Parse_MIME((char *)content.c_str(), content.size()), cleanVObject);
+
+    return 0;
+}
