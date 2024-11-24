e0b57ee28f4ebdc566cd72fa43cd8c8f9cec897c
Allen Winter <winter@kde.org>
2024-11-24 11:17:55 -0500

    src/libical/byref.c - cleaning

--- src/libical/byref.c
+++ src/libical/byref.c
@@ -1,6 +1,3 @@
-// There are multiple languages that only support pointers but not structures directly as interop.
-// This file functions as a mapper to oversee this.
-
 /*======================================================================
  FILE: byref.c
  CREATOR: Siam.Smeets 18 Nov 24
@@ -11,6 +8,9 @@
 
 ======================================================================*/
 
+// There are multiple languages that only support pointers but not structures directly as interop.
+// This file functions as a mapper to oversee this.
+
 /**
  * @file byref.c
  * @brief A set of function wrappers to handle non-structured return type interop.
@@ -18,20 +18,13 @@
  * Not all programming languages managed or not have support for c-structured return types.
  * To allow interop with these programming languages this file, over time, will contain wrapper functions,
  * That wrap the return type to a "byref" or pointer variable which is used as output.
- */
+*/
+
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
 
-#include "icalcomponent.h"
-#include "icaltime.h"
-#include "astime.h"
-#include "icalerror.h"
-#include "icalmemory.h"
 #include "icaltimezone.h"
-
-#include <ctype.h>
-#include <stdlib.h>
 
 /** @brief Convenience constructor (ret-wrapped)
  *
