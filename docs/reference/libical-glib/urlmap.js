56781e7cb253e2366accdcd14e9ef6c2955b868f
Corentin Noël <corentin.noel@collabora.com>
2024-11-26 14:37:54 +0100

    libical-glib: Relicense urlmap.js
    Take paternity and relicense as this is a trivial file.

--- docs/reference/libical-glib/urlmap.js
+++ docs/reference/libical-glib/urlmap.js
@@ -1,7 +1,5 @@
-// SPDX-FileCopyrightText: 2021 GNOME Foundation
-// SPDX-License-Identifier: LGPL-2.1-or-later
-
-// A map between namespaces and base URLs for their online documentation
+// SPDX-FileCopyrightText: 2024 Corentin Noël
+// SPDX-License-Identifier: LGPL-2.1-only OR MPL-2.0
 baseURLs = [
     [ 'GLib', 'https://docs.gtk.org/glib/' ],
     [ 'GObject', 'https://docs.gtk.org/gobject/' ],
