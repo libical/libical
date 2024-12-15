6fda6af5d5a310ce9d274002a6f080c2b520a12d
Markus Minichmayr <markus@tapkey.com>
2024-12-15 11:09:34 +0100

    Fix comment as `icalcomponent_foreach_recurrence` does support RDATEs as PERIODs since #707.

--- src/libical/icalcomponent.h
+++ src/libical/icalcomponent.h
@@ -452,7 +452,6 @@ LIBICAL_ICAL_EXPORT int icalproperty_recurrence_is_excluded(icalcomponent *comp,
  * It will filter out events that are specified as an EXDATE or an EXRULE.
  *
  * TODO: We do not filter out duplicate RRULES/RDATES
- * TODO: We do not handle RDATEs with explicit periods
  */
 LIBICAL_ICAL_EXPORT void icalcomponent_foreach_recurrence(icalcomponent *comp,
                                                           struct icaltimetype start,
