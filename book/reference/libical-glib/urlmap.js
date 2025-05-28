5c39d3ac81b1ad79e18d9099a3eaac86eba607c8
Allen Winter <winter@kde.org>
2025-05-28 11:54:59 -0400

    Update the Book

---
+++ book/reference/libical-glib/urlmap.js
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: 2024 Corentin Noël
+// SPDX-License-Identifier: LGPL-2.1-only OR MPL-2.0
+baseURLs = [
+    [ 'GLib', 'https://docs.gtk.org/glib/' ],
+    [ 'GObject', 'https://docs.gtk.org/gobject/' ],
+    [ 'Gio', 'https://docs.gtk.org/gio/' ],
+]
