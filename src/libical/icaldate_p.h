bdee086c82ffbfd3f53bbbde43e4332a347016dc
Allen Winter <winter@kde.org>
2025-09-02 08:22:42 -0400

    Move/rename caldat and juldat to private API
    * rename caldat to ical_caldate and juldat to ical_juldat
    * move them into private API header icaldate_p
    
    GH-848

--- src/libical/icaldate_p.h
+++ src/libical/icaldate_p.h
@@ -1,8 +1,27 @@
+/*======================================================================
+ FILE: icaldate_p.h
+
+ SPDX-FileCopyrightText: 1986-2000, Hiram Clawson
+ SPDX-License-Identifier: BSD-3-Clause
+======================================================================*/
+
+/*************************************************************************
+ * WARNING: USE AT YOUR OWN RISK                                         *
+ * These are library internal-only functions.                            *
+ * Be warned that these functions can change at any time without notice. *
+ *************************************************************************/
+
+#ifndef ICALDATE_P_H
+#define ICALDATE_P_H
+
+#include "libical_ical_export.h"
 /*
- * This work is based on work from Hiram Clawson and has been modified to the
- * needs of the libical project. The original copyright notice is as follows:
- */
-/*
+ * The ical_caldat and ical_juldat functions are based on work from Hiram Clawson
+ * and have been modified to the needs of the libical project. To distinguish between
+ * the original code and the modifications made, refer to the source code history.
+ *
+ * The original copyright notice is as follows:
+ *
  *      Copyright (c) 1986-2000, Hiram Clawson
  *      All rights reserved.
  *
@@ -10,19 +29,19 @@
  *      without modification, are permitted provided that the following
  *      conditions are met:
  *
- *              Redistributions of source code must retain the above
- *              copyright notice, this list of conditions and the
- *              following disclaimer.
+ *      Redistributions of source code must retain the above
+ *      copyright notice, this list of conditions and the
+ *      following disclaimer.
  *
- *              Redistributions in binary form must reproduce the
- *              above copyright notice, this list of conditions and
- *              the following disclaimer in the documentation and/or
- *              other materials provided with the distribution.
+ *      Redistributions in binary form must reproduce the
+ *      above copyright notice, this list of conditions and
+ *      the following disclaimer in the documentation and/or
+ *      other materials provided with the distribution.
  *
- *              Neither name of The Museum of Hiram nor the names of
- *              its contributors may be used to endorse or promote products
- *              derived from this software without specific prior
- *              written permission.
+ *      Neither name of The Museum of Hiram nor the names of
+ *      its contributors may be used to endorse or promote products
+ *      derived from this software without specific prior
+ *      written permission.
  *
  *      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
  *      CONTRIBUTORS ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
@@ -37,41 +56,15 @@
  *      IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  *      THE POSSIBILITY OF SUCH DAMAGE.
  */
-/*
- * The modifications made are licensed as follows (to distinguish between
- * the original code and the modifications made, refer to the source code
- * history):
- */
- /*======================================================================
 
-  SPDX-FileCopyrightText: 2018, Markus Minichmayr
-      https://tapkey.com
-  SPDX-License-Identifier: LGPL-2.1-only OR MPL-2.0
- ========================================================================*/
-
-/**
- *      @file astime.h
- *      @brief contains definitions of structures used for time calculations.
- */
-
-#ifndef ICAL_ASTIME_H
-#define ICAL_ASTIME_H
-
-#include "libical_ical_export.h"
-
-/*      Functions in caldate.c  */
-
-typedef struct ut_instant_int
-{
-    long j_date0;      /**< julian decimal date, 0 = 01 Jan 4713 BC */
-    long year;          /**< year, valid range [-4,713, +32,767] */
-    int month;          /**<    [1-12]  */
-    int day;            /**<    [1-31]  */
-    int weekday;                /**<    [0-6]   */
-    int day_of_year;            /**<    [1-366] */
+typedef struct ut_instant_int {
+    long j_date0;    /**< julian decimal date, 0 = 01 Jan 4713 BC */
+    long year;       /**< year, valid range [-4,713, +32,767] */
+    int month;       /**<    [1-12]  */
+    int day;         /**<    [1-31]  */
+    int weekday;     /**<    [0-6]   */
+    int day_of_year; /**<    [1-366] */
 } UTinstantInt, *UTinstantIntPtr;
-
-/*      Functions in caldate.c  */
 
 /**
  *	Computes the day of the week, the day of the year from
@@ -91,7 +84,7 @@ typedef struct ut_instant_int
  *	50 Year canon of solar eclipses: 1986-2035
  *
  */
-LIBICAL_ICAL_EXPORT void caldat_int(UTinstantIntPtr);
+LIBICAL_ICAL_EXPORT void ical_caldat(UTinstantIntPtr);
 
 /**
  *	Computes the julian decimal date (j_date) from
@@ -108,6 +101,6 @@ LIBICAL_ICAL_EXPORT void caldat_int(UTinstantIntPtr);
  *	from fortran program by F. Espenak - April 1982 Page 276,
  *	50 Year canon of solar eclipses: 1986-2035
  */
-LIBICAL_ICAL_EXPORT void juldat_int(UTinstantIntPtr);
+LIBICAL_ICAL_EXPORT void ical_juldat(UTinstantIntPtr);
 
 #endif
