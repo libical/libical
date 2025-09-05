97811ed9333b2be3eff756ac573afa0d862671ea
Allen Winter <winter@kde.org>
2025-09-05 06:58:58 -0400

    Remove the ICAL_ERRORS_ARE_FATAL conditional compile macro (#986)
    always use the icalerror_set_errno function but compile it as an inline
    
    fixes: #984

--- src/libical/icalerror.c
+++ src/libical/icalerror.c
@@ -111,7 +111,6 @@ int icalerror_get_errors_are_fatal(void)
     return icalerror_errors_are_fatal;
 }
 
-#if defined(ICAL_SETERROR_ISFUNC)
 void icalerror_set_errno(icalerrorenum x)
 {
     icalerrno = x;
@@ -122,8 +121,6 @@ void icalerror_set_errno(icalerrorenum x)
         icalassert(0);
     }
 }
-
-#endif
 
 struct icalerror_state {
     icalerrorenum error;
