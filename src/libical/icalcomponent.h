a084e6271e210dd1557d2edd2a833464489459d7
Collin Mendia <collin.mendia@gmail.com>
2025-09-17 20:11:38 -0400

    icalcomponent: filter duplicate RDATEs

--- src/libical/icalcomponent.h
+++ src/libical/icalcomponent.h
@@ -461,9 +461,6 @@ LIBICAL_ICAL_EXPORT bool icalproperty_recurrence_is_excluded(icalcomponent *comp
  * value.
  *
  * It will filter out events that are specified as an EXDATE or an EXRULE.
- *
- * TODO: We do not filter out duplicate RRULES/RDATES
- * TODO: We do not differentiate between nominal and exact durations.
  */
 LIBICAL_ICAL_EXPORT void icalcomponent_foreach_recurrence(icalcomponent *comp,
                                                           struct icaltimetype start,
