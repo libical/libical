a46158d10a324a5378d8c44bea74cc57fa97a8be
Allen Winter <winter@kde.org>
2025-10-01 09:09:39 -0400

    icalspanlist - move the icalspanlist_impl structure def to the header (#1054)

--- src/libicalss/icalspanlist.c
+++ src/libicalss/icalspanlist.c
@@ -15,12 +15,6 @@
 
 #include <stdlib.h>
 
-struct icalspanlist_impl {
-    icalpvl_list spans;        /**< list of icaltime_span data **/
-    struct icaltimetype start; /**< start time of span **/
-    struct icaltimetype end;   /**< end time of span **/
-};
-
 /** @brief Internal comparison function for two spans
  *
  *  @param  a   a spanlist.
