599ab41586175e178d805ccf6861c779dd19f6c2
Allen Winter <winter@kde.org>
2026-01-29 08:24:51 -0500

    scripts/compileit.sh - build statically if the -s option is specified

--- scripts/compileit.sh
+++ scripts/compileit.sh
@@ -10,6 +10,23 @@ set -e
 #If any process in a pipeline fails, the return value is a failure.
 set -o pipefail
 
+USAGE() {
+  echo "Usage: $(basename "$0") [-s]"
+  echo "where, the -s means to build statically"
+  exit 1
+}
+
+staticBuild=0
+if (test $# -gt 1); then
+  USAGE
+elif (test $# -eq 1); then
+  if (test "$1" == "-s"); then
+    staticBuild=1
+  else
+    USAGE
+  fi
+fi
+
 TOP=$(readlink -nf "$0")
 TOP=$(dirname "$TOP")
 cd "$TOP/.."
@@ -34,63 +51,65 @@ if (test "$(uname -s)" = "Darwin"); then
   export XML_CATALOG_FILES=/usr/local/etc/xml/catalog
 fi
 
-rm -rf "$BDIR"
-mkdir -p "$BDIR" &&
-  cd "$BDIR"
-# shellcheck disable=SC2086,SC2226
-cmake -S .. \
-  $CMAKE_STRICT \
-  -G Ninja \
-  -DCMAKE_BUILD_TYPE=Debug \
-  -DLIBICAL_STATIC=OFF \
-  -DLIBICAL_ENABLE_BUILTIN_TZDATA=OFF \
-  -DLIBICAL_BUILD_TESTING=ON \
-  -DLIBICAL_BUILD_DOCS=ON \
-  -DLIBICAL_BUILD_EXAMPLES=ON \
-  -DLIBICAL_CXX_BINDINGS=ON \
-  -DLIBICAL_GOBJECT_INTROSPECTION=ON \
-  -DLIBICAL_GLIB_VAPI=ON \
-  -DLIBICAL_GLIB_BUILD_DOCS=ON \
-  $CMAKE_VERSION4_OPTIONS \
-  -DLIBICAL_BUILD_TESTING_BIGFUZZ=ON \
-  -DCMAKE_INSTALL_PREFIX="$INSTALLDIR" &&
-  (cd "$TOP" && ln -sf "$BDIR/compile_commands.json") &&
-  ninja &&
-  ninja test &&
-  ninja install &&
-  ninja docs &&
-  ninja build-book
-if (test $? -ne 0); then exit; fi
-ninja uninstall && rm -rf "$INSTALLDIR"
+if (test $staticBuild -eq 0); then
+  rm -rf "$BDIR"
+  mkdir -p "$BDIR" &&
+    cd "$BDIR"
+  # shellcheck disable=SC2086,SC2226
+  cmake -S .. \
+    $CMAKE_STRICT \
+    -G Ninja \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DLIBICAL_STATIC=OFF \
+    -DLIBICAL_ENABLE_BUILTIN_TZDATA=OFF \
+    -DLIBICAL_BUILD_TESTING=ON \
+    -DLIBICAL_BUILD_DOCS=ON \
+    -DLIBICAL_BUILD_EXAMPLES=ON \
+    -DLIBICAL_CXX_BINDINGS=ON \
+    -DLIBICAL_GOBJECT_INTROSPECTION=ON \
+    -DLIBICAL_GLIB_VAPI=ON \
+    -DLIBICAL_GLIB_BUILD_DOCS=ON \
+    $CMAKE_VERSION4_OPTIONS \
+    -DLIBICAL_BUILD_TESTING_BIGFUZZ=ON \
+    -DCMAKE_INSTALL_PREFIX="$INSTALLDIR" &&
+    (cd "$TOP" && ln -sf "$BDIR/compile_commands.json") &&
+    ninja &&
+    ninja test &&
+    ninja install &&
+    ninja docs &&
+    ninja build-book
+  if (test $? -ne 0); then exit; fi
+  ninja uninstall && rm -rf "$INSTALLDIR"
 
-#static build testing
-BDIR="$BDIR-static"
-INSTALLDIR="$INSTALLDIR-static"
+else #static build
+  BDIR="$BDIR-static"
+  INSTALLDIR="$INSTALLDIR-static"
 
-#rm -rf "$BDIR"
-mkdir -p "$BDIR" &&
-  cd "$BDIR"
-# shellcheck disable=SC2086,SC2226
-cmake -S .. \
-  -G Ninja \
-  -DCMAKE_BUILD_TYPE=Debug \
-  -DLIBICAL_STATIC=ON \
-  -DLIBICAL_BUILD_TESTING=ON \
-  -DLIBICAL_BUILD_DOCS=ON \
-  -DLIBICAL_BUILD_EXAMPLES=ON \
-  -DLIBICAL_CXX_BINDINGS=ON \
-  -DLIBICAL_JAVA_BINDINGS=OFF \
-  -DLIBICAL_GOBJECT_INTROSPECTION=OFF \
-  -DLIBICAL_GLIB_VAPI=OFF \
-  -DLIBICAL_GLIB_BUILD_DOCS=OFF \
-  $CMAKE_VERSION4_OPTIONS \
-  -DLIBICAL_BUILD_TESTING_BIGFUZZ=ON \
-  -DCMAKE_INSTALL_PREFIX="$INSTALLDIR" &&
-  (cd "$TOP" && ln -sf "$BDIR/compile_commands.json") &&
-  ninja &&
-  ninja test &&
-  ninja install &&
-  ninja docs &&
-  ninja build-book
-if (test $? -ne 0); then exit; fi
-ninja uninstall && rm -rf "$INSTALLDIR"
+  #rm -rf "$BDIR"
+  mkdir -p "$BDIR" &&
+    cd "$BDIR"
+  # shellcheck disable=SC2086,SC2226
+  cmake -S .. \
+    -G Ninja \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DLIBICAL_STATIC=ON \
+    -DLIBICAL_BUILD_TESTING=ON \
+    -DLIBICAL_BUILD_DOCS=ON \
+    -DLIBICAL_BUILD_EXAMPLES=ON \
+    -DLIBICAL_CXX_BINDINGS=ON \
+    -DLIBICAL_JAVA_BINDINGS=OFF \
+    -DLIBICAL_GOBJECT_INTROSPECTION=OFF \
+    -DLIBICAL_GLIB_VAPI=OFF \
+    -DLIBICAL_GLIB_BUILD_DOCS=OFF \
+    $CMAKE_VERSION4_OPTIONS \
+    -DLIBICAL_BUILD_TESTING_BIGFUZZ=ON \
+    -DCMAKE_INSTALL_PREFIX="$INSTALLDIR" &&
+    (cd "$TOP" && ln -sf "$BDIR/compile_commands.json") &&
+    ninja &&
+    ninja test &&
+    ninja install &&
+    ninja docs &&
+    ninja build-book
+  if (test $? -ne 0); then exit; fi
+  ninja uninstall && rm -rf "$INSTALLDIR"
+fi
