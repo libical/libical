9af3e9755e91af03d2a8c5f5f92c3bb43cbf7e89
Allen Winter <winter@kde.org>
2026-01-30 11:16:02 -0500

    Start adding fuzzer testing for the vcard library

---
+++ src/test/libicalvcard/vcardcomponent_fuzz1.c
@@ -0,0 +1,79 @@
+/*======================================================================
+ *
+ * SPDX-FileCopyrightText: Allen Winter <winter@kde.org>
+ * SPDX-License-Identifier: LGPL-2.1-only OR MPL-2.0
+ *
+ * Based on the original code by Gabe Sherman in
+ * https://github.com/libical/libical/issues/678
+ * ======================================================================*/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#if defined(NDEBUG)
+#undef NDEBUG
+#endif
+
+#include <stdlib.h>
+
+#include "libicalvcard/vcard.h"
+
+int main(int argc, const char *argv[])
+{
+    FILE *fp;
+    int fd, r;
+    const char *fname;
+#if defined(HAVE__FSTAT64)
+    struct _stat64 sbuf;
+#else
+    struct stat sbuf;
+#endif
+    size_t filesize;
+    void *data = NULL;
+    vcardcomponent *card;
+
+    if (argc != 2) {
+        fprintf(stderr, "Error: must supply a test file name\n");
+        return 1;
+    } else {
+        fname = argv[1];
+    }
+
+    fp = fopen(fname, "rb");
+    if (fp == (FILE *)NULL) {
+        fprintf(stderr, "Error: unable to open %s\n", fname);
+        return 1;
+    }
+
+    fd = fileno(fp);
+    if (fstat(fd, &sbuf) != 0) {
+        fprintf(stderr, "Error: unable to fstat %s\n", fname);
+        fclose(fp);
+        return 1;
+    }
+    filesize = (size_t)sbuf.st_size;
+    data = malloc(filesize + 1);
+    if (!data) {
+        fprintf(stderr, "Error: unable to allocate memory\n");
+        free(data);
+        fclose(fp);
+        return 1;
+    }
+    memset(data, 0, filesize + 1);
+
+    r = read(fd, data, filesize);
+    fclose(fp);
+
+    if (r < 0) {
+        fprintf(stderr, "Error: Failed to read data\n");
+        free(data);
+        return 1;
+    }
+
+    card = vcardparser_parse_string(data);
+
+    vcardcomponent_free(card);
+
+    return 0;
+}
