aaabf7dc594100adaff47d2e69d87dedfa33f179
Allen Winter <winter@kde.org>
2026-03-18 10:07:59 -0400

    Make some icaltimezone_array_* functions invisible (#1278)
    These are no longer exported for public consumption.
     * icaltimezone_array_new
     * icaltimezone_array_append_from_vtimezone
     * icaltimezone_array_free
    
    GH-848

--- src/test/libical-glib/timezone.py
+++ src/test/libical-glib/timezone.py
@@ -116,14 +116,5 @@ timezone = ICalGLib.Timezone.new()
 timezone.set_component(ICalGLib.Component.clone(component))
 assert timezone.get_location() == la.get_location()
 
-array = ICalGLib.Timezone.array_new()
-ICalGLib.Timezone.array_append_from_vtimezone(array, ICalGLib.Component.clone(la.get_component()))
-ICalGLib.Timezone.array_append_from_vtimezone(array, ICalGLib.Component.clone(chicago.get_component()))
-assert array.size() == 2
-timezone1 = ICalGLib.Timezone.array_element_at(array, 0)
-assert timezone1.get_display_name() == la.get_display_name()
-timezone2 = ICalGLib.Timezone.array_element_at(array, 1)
-assert timezone2.get_display_name() == chicago.get_display_name()
-
 ICalGLib.Timezone.free_builtin_timezones()
 ICalGLib.Object.free_global_objects()
