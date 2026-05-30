3b81ea2d0cccc0491334f6906778144d4f656990
Allen Winter <winter@kde.org>
2026-05-30 07:47:00 -0400

    Update the Book README

--- book/searcher-c2a407aa.js
+++ book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b1f29d7f.js',
+                path_to_root + 'searchindex-75b23c23.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
