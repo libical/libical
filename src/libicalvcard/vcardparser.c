1a92134dba6dd5f3f4d53d9745b5b2a38d1c29f1
Allen Winter <winter@kde.org>
2026-07-29 07:11:06 -0400

    Fix libicalvcard out-of-memory fuzz issue
    Don't allow vcard prop names longer than 100 chars.
    
    see https://oss-fuzz.com/testcase-detail/6073213411721216

--- src/libicalvcard/vcardparser.c
+++ src/libicalvcard/vcardparser.c
@@ -561,7 +561,9 @@ static int _parse_prop_name(struct vcardparser_state *state)
 
     NOTESTART();
 
-    while (*state->p) {
+    size_t cnt = 0;
+    const size_t max_chars = 100; // plenty long enough for any property name
+    while (*state->p && cnt++ < max_chars) {
         /* Handle control characters and break for NUL char */
         HANDLECTRL(state);
 
